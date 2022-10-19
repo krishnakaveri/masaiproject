f1c447eb0c1f7448b68617ddb4c258e3f6da0723
TusharBhavsar18 <tusharbhavsar2011@gmail.com>
2022-10-19 13:29:28 +0530

    code fixed

--- coding.js
+++ coding.js
@@ -1,15 +1,22 @@
-function identifyPrime(num) {
-   let count=0;
-    for(i=0;i<=num;i++){
-        if(num%i==0){
+//Identify_Prime...
+
+let number = 18;
+
+let  count = 0;
+    
+    for(let i=1 ; i<=number ; i++){
+        
+        if(number % i == 0){
+
             count++;
         }
-    }
-        if(count==2){
-            console.log("Yes");
 
-        }else{
-            console.log("No");
-                
-}  
-}
+    } if(count == 2){
+        
+        console.log("Yes");
+
+    }else{
+
+        console.log("No");
+
+    }
